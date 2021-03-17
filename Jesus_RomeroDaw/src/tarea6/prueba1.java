a34a058310566f0919b6919e21c5becb68ced582
jromerov12 <jromerov12@iesalbarregas.es>
2021-03-17 18:06:10 +0100

    Validamos la clase prueba1 de la tarea 6

---
+++ Jesus_RomeroDaw/src/tarea6/prueba1.java
@@ -0,0 +1,9 @@
+package tarea6;
+
+public class prueba1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Prueba Tarea 6 Entornos");
+		}
+
+}
