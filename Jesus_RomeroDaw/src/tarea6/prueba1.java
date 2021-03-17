cab6d46417b2319be0f1c9cf02c253921b0ae5f5
jromerov12 <jromerov12@iesalbarregas.es>
2021-03-17 20:48:06 +0100

    validamos 3

--- Jesus_RomeroDaw/src/tarea6/prueba1.java
+++ Jesus_RomeroDaw/src/tarea6/prueba1.java
@@ -4,7 +4,8 @@ public class prueba1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Prueba Tarea 6 Entornos");
-		System.out.println("Prueba Tarea 6 Entornos cambios");
+		System.out.println("Prueba Tarea 6 Entornos cambios 1 ");
+		System.out.println("Prueba Tarea 6 Entornos cambios 2 ");
 		}
 
 }
