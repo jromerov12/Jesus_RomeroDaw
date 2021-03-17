f1a69ef80bd5b449b8e782c9340c7642781ebf6e
jromerov12 <jromerov12@iesalbarregas.es>
2021-03-17 20:59:29 +0100

    Se ha creado la rama clasenueva

---
+++ Jesus_RomeroDaw/src/tarea6/Nuevaclase.java
@@ -0,0 +1,10 @@
+package tarea6;
+
+public class Nuevaclase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("En la rama. Se crea la clase nueva");
+	}
+
+}
